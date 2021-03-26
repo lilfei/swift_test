598aedae023bf92e3805f5b9f3db1ac0da9f918a
李龙飞 <lilongfei@192.168.229.137>
2021-03-25 23:54:51 -0700

    first commit

---
+++ swift_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_test
+//
+//  Created by 李龙飞 on 2021/3/25.
+//  Copyright © 2021 李龙飞. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
